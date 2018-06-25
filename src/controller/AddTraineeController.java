440db7915b7e84804dc1b6afaf547a4bec4fb214
HungHuynhDang <huynhdanghung1996@gmail.com>
2018-06-25 14:48:36 +0700

    Matthew committed

--- src/controller/AddTraineeController.java
+++ src/controller/AddTraineeController.java
@@ -18,74 +18,84 @@ import model.bo.UserBo;
 @MultipartConfig
 public class AddTraineeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
-    public AddTraineeController() {
-        super();
-    }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/addTrainee.jsp");
-		rd.forward(request, response); 
+	public AddTraineeController() {
+		super();
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/addTrainee.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
+
 		System.out.println(request.getParameter("dateOfBirth"));
-		User trainee = new User(0, request.getParameter("username"), request.getParameter("password"), request.getParameter("fullname"), FormatDateLibrary.ConvertStringToDateSQL(request.getParameter("dateOfBirth")) , request.getParameter("email"), FormatDateLibrary.ConvertDateUntilToDateSQL(new Date()), 2, Integer.parseInt(request.getParameter("gender")), request.getParameter("address"), request.getParameter("phone"), "", "");
-		
+		User trainee = new User(0, request.getParameter("username"), request.getParameter("password"),
+				request.getParameter("fullname"),
+				FormatDateLibrary.ConvertStringToDateSQL(request.getParameter("dateOfBirth")),
+				request.getParameter("email"), FormatDateLibrary.ConvertDateUntilToDateSQL(new Date()), 2,
+				Integer.parseInt(request.getParameter("gender")), request.getParameter("address"),
+				request.getParameter("phone"), "", "");
+
 		UserBo userBo = new UserBo();
 
-	 	if (userBo.checkUsernameAlreadyExists(request.getParameter("username"))) {
+		if (userBo.checkUsernameAlreadyExists(request.getParameter("username"))) {
 
 			request.setAttribute("trainee", trainee);
 			request.setAttribute("error", " This username is already exists in system");
 			RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/addTrainee.jsp");
-			rd.forward(request, response); 
-			
-	 	} else if (userBo.checkEmailAlreadyExists(request.getParameter("email"))) {
+			rd.forward(request, response);
+
+		} else if (userBo.checkEmailAlreadyExists(request.getParameter("email"))) {
 
 			request.setAttribute("trainee", trainee);
 			request.setAttribute("error", " This email is already exists in system");
 			RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/addTrainee.jsp");
-			rd.forward(request, response); 
-			
-	 	}else { if ( userBo.checkTraineeInformation(request.getParameter("username"), request.getParameter("password"), request.getParameter("fullname"), request.getParameter("dateOfBirth") , request.getParameter("email"), request.getParameter("address"), request.getParameter("phone")) == false){
-			 	
+			rd.forward(request, response);
+
+		} else {
+			if (userBo.checkTraineeInformation(request.getParameter("username"), request.getParameter("password"),
+					request.getParameter("fullname"), request.getParameter("dateOfBirth"),
+					request.getParameter("email"), request.getParameter("address"),
+					request.getParameter("phone")) == false) {
+
 				request.setAttribute("error", " Please complete all information");
 				request.setAttribute("trainee", trainee);
 				RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/addTrainee.jsp");
-				rd.forward(request, response); 
-				
-		 } else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 0) {
-			 
-			 trainee.setAvatar("default.jpg");
-			 
+				rd.forward(request, response);
+
+			} else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 0) {
+
+				trainee.setAvatar("default.jpg");
+
 			} else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 1) {
 
 				request.setAttribute("trainee", trainee);
 				request.setAttribute("error", " Please add file jpg, png, gif");
 				RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/addTrainee.jsp");
-				rd.forward(request, response); 
-				
+				rd.forward(request, response);
+
 			} else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 2) {
-			 	
+
 				trainee.setAvatar(userBo.addTraineeAvatar(request.getPart("avatar"), request));
 			}
 
-	 		trainee.setPassword(MD5Library.md5(trainee.getPassword()));
-	 		
-			if ( userBo.addTrainee(trainee) > 0) {
+			trainee.setPassword(MD5Library.md5(trainee.getPassword()));
+
+			if (userBo.addTrainee(trainee) > 0) {
 				response.sendRedirect(request.getContextPath() + "/trainee/index?msg=1");
 			} else {
 				request.setAttribute("trainee", trainee);
 				request.setAttribute("error", " Can't add trainee. please try again later");
 				RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/addTrainee.jsp");
-				rd.forward(request, response); 
+				rd.forward(request, response);
 			}
-				
+
 		}
 	}
 
--- src/controller/EditTraineeController.java
+++ src/controller/EditTraineeController.java
@@ -17,24 +17,26 @@ import model.bo.UserBo;
 @MultipartConfig
 public class EditTraineeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EditTraineeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public EditTraineeController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		UserBo userBo = new UserBo();
 		int traineeId = Integer.parseInt(request.getParameter("id"));
 		request.setAttribute("trainee", userBo.getTraineeById(traineeId));
 		RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
-		rd.forward(request, response); 
+		rd.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 
 		UserBo userBo = new UserBo();
@@ -45,66 +47,68 @@ public class EditTraineeController extends HttpServlet {
 		trainee.setGender(Integer.parseInt(request.getParameter("gender")));
 		trainee.setPhone(request.getParameter("phone"));
 		trainee.setDateOfBirth(FormatDateLibrary.ConvertStringToDateSQL(request.getParameter("dateOfBirth")));
-		
-		if (!"".equals(request.getParameter("oldpass")))  {
-			if ( trainee.getPassword().equals(MD5Library.md5(request.getParameter("oldpass")))) {
-				
-				String newpass  = request.getParameter("newpass");
+
+		if (!"".equals(request.getParameter("oldpass"))) {
+			if (trainee.getPassword().equals(MD5Library.md5(request.getParameter("oldpass")))) {
+
+				String newpass = request.getParameter("newpass");
 				String confimpass = request.getParameter("confirmpass");
-				if (newpass.equals(confimpass) ) {
+				if (newpass.equals(confimpass)) {
 					trainee.setPassword(MD5Library.md5(request.getParameter("oldpass")));
 				} else {
 
 					request.setAttribute("trainee", trainee);
 					request.setAttribute("error", " Two new password not the same");
 					RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
-					rd.forward(request, response); 
+					rd.forward(request, response);
 				}
 			} else {
 
 				request.setAttribute("trainee", trainee);
 				request.setAttribute("error", " Your password is not correct");
 				RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
-				rd.forward(request, response); 
+				rd.forward(request, response);
 			}
 		}
-		
+
 		if (userBo.checkUsernameAlreadyExistsEdit(request.getParameter("username"), trainee.getUserId())) {
 
 			request.setAttribute("trainee", trainee);
 			request.setAttribute("error", " This username is already exists in system");
 			RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
-			rd.forward(request, response); 
-			
-	 	} else if ( userBo.checkTraineeInformation(request.getParameter("username"), trainee.getPassword(), request.getParameter("fullname"), request.getParameter("dateOfBirth") , trainee.getEmail(), request.getParameter("address"), request.getParameter("phone")) == false){
-			 	
-				request.setAttribute("error", " Please complete all information");
-				request.setAttribute("trainee", trainee);
-				RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
-				rd.forward(request, response); 
-				
-		 }else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 0) {
-			 
-			}else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 1) {
+			rd.forward(request, response);
 
-				request.setAttribute("trainee", trainee);
-				request.setAttribute("error", " Please add file jpg, png, gif");
-				RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
-				rd.forward(request, response); 
-				
-			} else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 2) {
-			 	
-				trainee.setAvatar(userBo.addTraineeAvatar(request.getPart("avatar"), request));
-			}	
+		} else if (userBo.checkTraineeInformation(request.getParameter("username"), trainee.getPassword(),
+				request.getParameter("fullname"), request.getParameter("dateOfBirth"), trainee.getEmail(),
+				request.getParameter("address"), request.getParameter("phone")) == false) {
 
-			if ( userBo.editTrainee(trainee) > 0) {
-				response.sendRedirect(request.getContextPath() + "/trainee/index?msg=1");
-			} else {
-				request.setAttribute("trainee", trainee);
-				request.setAttribute("error", " Can't edit trainee. please try again later");
-				RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
-				rd.forward(request, response); 
-			}
+			request.setAttribute("error", " Please complete all information");
+			request.setAttribute("trainee", trainee);
+			RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
+			rd.forward(request, response);
+
+		} else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 0) {
+
+		} else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 1) {
+
+			request.setAttribute("trainee", trainee);
+			request.setAttribute("error", " Please add file jpg, png, gif");
+			RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
+			rd.forward(request, response);
+
+		} else if (userBo.checkAddTraineeAvatar(request.getPart("avatar"), request) == 2) {
+
+			trainee.setAvatar(userBo.addTraineeAvatar(request.getPart("avatar"), request));
 		}
+
+		if (userBo.editTrainee(trainee) > 0) {
+			response.sendRedirect(request.getContextPath() + "/trainee/index?msg=1");
+		} else {
+			request.setAttribute("trainee", trainee);
+			request.setAttribute("error", " Can't edit trainee. please try again later");
+			RequestDispatcher rd = request.getRequestDispatcher("/admin/trainees/edit.jsp");
+			rd.forward(request, response);
+		}
+	}
 
 }
