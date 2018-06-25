eb8d22127ac998670b10d5f7f93addb576847133
Nam2402 <nguyenngocnam242@gmail.com>
2018-06-25 09:45:49 +0700

    check trainer schedule by Schedule.java

---
+++ src/model/bean/Schedule.java
@@ -0,0 +1,119 @@
+package model.bean;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public class Schedule {
+	private String nameclass;
+	private String course;
+	private int trainerId;
+	private String fullname;
+	private Date createAt;
+	private String timeOfDate;
+	private String dateOfWeek;
+	private int countLession;
+	private String nameroom;
+	private int classid;
+	
+	
+
+
+	public String getNameclass() {
+		return nameclass;
+	}
+	public void setNameclass(String nameclass) {
+		this.nameclass = nameclass;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public int getTrainerId() {
+		return trainerId;
+	}
+
+	public void setTrainerId(int trainerId) {
+		this.trainerId = trainerId;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public String getTimeOfDate() {
+		return timeOfDate;
+	}
+
+	public void setTimeOfDate(String timeOfDate) {
+		this.timeOfDate = timeOfDate;
+	}
+
+	public String getDateOfWeek() {
+		return dateOfWeek;
+	}
+
+	public void setDateOfWeek(String dateOfWeek) {
+		this.dateOfWeek = dateOfWeek;
+	}
+
+	public int getCountLession() {
+		return countLession;
+	}
+
+	public void setCountLession(int countLession) {
+		this.countLession = countLession;
+	}
+
+	public String getNameroom() {
+		return nameroom;
+	}
+
+	public void setNameroom(String nameroom) {
+		this.nameroom = nameroom;
+	}
+
+	public int getClassid() {
+		return classid;
+	}
+
+	public void setClassid(int classid) {
+		this.classid = classid;
+	}
+
+	public Schedule() {
+		super();
+	}
+
+	public Schedule(String nameclass, String course, int trainerId, String fullname, Date createAt, String timeOfDate,
+			String dateOfWeek, int countLession, String nameroom, int classid) {
+		super();
+		this.nameclass = nameclass;
+		this.course = course;
+		this.trainerId = trainerId;
+		this.fullname = fullname;
+		this.createAt = createAt;
+		this.timeOfDate = timeOfDate;
+		this.dateOfWeek = dateOfWeek;
+		this.countLession = countLession;
+		this.nameroom = nameroom;
+		this.classid = classid;
+	}
+
+}
